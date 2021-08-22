9a4e641f40cdaeddfd526317300396a688f770e0
maroust1 <71834145+1000101cz@users.noreply.github.com>
2021-08-22 22:20:34 +0000

    Update prerequisites for Debian and Arch

---
+++ prerequisites-arch.sh
@@ -0,0 +1,5 @@
+#required packages for Arch Linux
+
+pacman -S sdl sdl2 sdl2_image sox python-pip ffmpeg
+
+pip install psutil
---
+++ prerequisites-deb.sh
@@ -0,0 +1,5 @@
+#required packages for Ubuntu (Debian)
+
+apt install libsdl2-dev libsdl2-image-2.0-0 libsdl2-image-dev sox python3-pip ffmpeg
+
+pip install psutil
